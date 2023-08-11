067c86b9fe4c15b69da6ed55fc1263d31f28c1ba
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2023-08-11 10:01:36 +0000

    pipes

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -4,6 +4,7 @@ interface Book {
   name: string;
   author: string;
   image: string;
+  amount: number
 }
 
 @Component({
@@ -16,13 +17,15 @@ export class BooksComponent {
   books: Book[] = [
     {
       name: 'Clean Code',
-      author: 'Robert C Martin',
+      author: 'robert c martin',
       image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
+      amount: 700,
     },
     {
       name: 'The Pragmatic Programmer',
-      author: 'David Thomas',
+      author: 'david thomas',
       image: 'https://m.media-amazon.com/images/I/51A8l+FxFNL.jpg',
+      amount: 800,
     }
   ]
 
